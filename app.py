2468bccd666b49165a3219261f36131e94252f99
Benoily <fbenoily@yahoo.com>
2023-03-03 20:26:14 +0100

    first commit

---
+++ app.py
@@ -0,0 +1,63 @@
+from flask import Flask,request,render_template,url_for
+import tensorflow
+from tensorflow import keras
+from tensorflow.keras.preprocessing.image import load_img, img_to_array
+import os
+#import pickle
+import numpy as np
+import json
+
+IMG_WIDTH_HEIGHT = (256, 256)
+
+# Fonctions loss
+def dice_coeff(y_true, y_pred):
+    smooth = 1.
+    y_true_f = K.cast(K.flatten(y_true), K.floatx())
+    y_pred_f = K.cast(K.flatten(y_pred), K.floatx())
+    intersection = K.sum(y_true_f * y_pred_f)
+    score = (2. * intersection + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) + smooth)
+    return score
+
+def dice_loss(y_true, y_pred):
+    loss = 1 - dice_coeff(y_true, y_pred)
+    return loss
+
+def total_loss(y_true, y_pred):
+    loss = categorical_crossentropy(y_true, y_pred) + (3*dice_loss(y_true, y_pred))
+    return loss
+
+# Load model
+model = keras.models.load_model('model/model_unet_dice_aug.h5', custom_objects={'total_loss': total_loss}, compile=True)
+
+app =Flask(__name__)
+
+
+@app.route('/')
+def index():
+    image_list = os.listdir('data/test/images')
+    return render_template('index.html', image_list=image_list)
+
+#@app.route('/')
+#def index():
+#    return render_template('index.html')
+
+@app.route('/',methods=['GET','POST'])
+def home():
+    if request.method =='POST':
+        # Retourner le fichier sélectionné
+        image = request.form['image']
+        image_path = str('data/test/images/' + image)
+ 
+        #image=os.path.join(app.config['UPLOAD_FOLDER'],image)
+        X = img_to_array(load_img(image_path, target_size=(IMG_WIDTH_HEIGHT)))/255
+        X = np.expand_dims(X, 0)
+        prediction = model.predict(X)
+        pred_mask = np.argmax(prediction, axis=-1)
+        pred_mask = np.squeeze(pred_mask)
+        print(pred_mask)
+        
+    return render_template('index.html',prediction=pred_mask)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
