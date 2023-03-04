ecf9d2ca3e66dd4b34cc2d0dea1945ef3373c3c4
Benoily <122854359+Benoily@users.noreply.github.com>
2023-03-04 13:19:05 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,61 +1,51 @@
-from flask import Flask,request,render_template,url_for
-import tensorflow
-from tensorflow import keras
-from tensorflow.keras.preprocessing.image import load_img, img_to_array
-import os
-#import pickle
+""http://web.univ-ubs.fr/lmba/lardjane/python/c4.pdf -> page 260"""
+
+from flask import Flask, request, jsonify, Response
 import numpy as np
-import json
-
-IMG_WIDTH_HEIGHT = (256, 256)
-
-# Fonctions loss
-def dice_coeff(y_true, y_pred):
-    smooth = 1.
-    y_true_f = K.cast(K.flatten(y_true), K.floatx())
-    y_pred_f = K.cast(K.flatten(y_pred), K.floatx())
-    intersection = K.sum(y_true_f * y_pred_f)
-    score = (2. * intersection + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) + smooth)
-    return score
-
-def dice_loss(y_true, y_pred):
-    loss = 1 - dice_coeff(y_true, y_pred)
-    return loss
-
-def total_loss(y_true, y_pred):
-    loss = categorical_crossentropy(y_true, y_pred) + (3*dice_loss(y_true, y_pred))
-    return loss
-
-# Load model
-model = keras.models.load_model('model/model_unet_dice_aug.h5', custom_objects={'total_loss': total_loss}, compile=True)
-
-app =Flask(__name__)
+import cv2
+import os
+import segmentation_models as sm
 
 
-@app.route('/')
-def index():
-    image_list = os.listdir('data/test/images')
-    return render_template('index.html', image_list=image_list)
+app = Flask(__name__)
+PORT = os.environ.get("PORT")
 
-#@app.route('/')
-#def index():
-#    return render_template('index.html')
+# Chargement du modèle
+model = sm.Unet('vgg16', classes=8)
+model.load_weights('unet_vgg16_aug_weights.h5')
 
-@app.route('/',methods=['POST'])
-def home():
-    if request.method =='POST':
-        # Retourner le fichier sélectionné
-        image = request.form['image']
-        image_path = str('data/test/images/' + image)
-        X = img_to_array(load_img(image_path, target_size=(IMG_WIDTH_HEIGHT)))/255
-        X = np.expand_dims(X, 0)
-        prediction = model.predict(X)
-        pred_mask = np.argmax(prediction, axis=-1)
-        pred_mask = np.squeeze(pred_mask)
-        print(pred_mask)
+
+# defining a route for only post requests
+@app.route('/predict', methods=['POST'])
+def predict():
+    response = {}
+    try:
+        r = request
+        # convert string of image data to uint8
+        nparr = np.frombuffer(r.data, np.uint8)
+        # decode image
+        img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+
+        # build a response dict to send back to client
+        response = {'message': 'image received. size={}x{}'.format(
+            img.shape[1], img.shape[0])}
         
-    return render_template('index.html',prediction=pred_mask)
+        img = img/255
+        x = cv2.resize(img, (512, 256))
+        pred = model.predict(np.expand_dims(x, axis=0))
+        pred_mask = np.argmax(pred, axis=-1)
+        pred_mask = np.expand_dims(pred_mask, axis=-1)
+        pred_mask = np.squeeze(pred_mask)
+
+        # creating a response object
+        # storing the model's prediction in the object
+        response['prediction'] =  pred_mask.tolist()
+    except Exception as e:
+        response['error'] = e
+
+    # returning the response object as json
+    return jsonify(response)
 
 
-if __name__ == '__main__':
-    app.run(debug=False)
+if __name__ == "__main__":
+     app.run(debug=True)
