9b9a6a7d2af768a53379ce83baff5ceb57b81ede
Benoily <122854359+Benoily@users.noreply.github.com>
2023-03-05 05:10:58 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,17 @@
 from flask import Flask,request,render_template,url_for
 import tensorflow
 from tensorflow import keras
-from tensorflow.keras.preprocessing import image
-from tensorflow.keras.preprocessing.image import load_img, img_to_array
+#from tensorflow.keras.preprocessing.image import load_img, img_to_array
 import os
+from PIL import Image
+import base64
+from io import BytesIO
 #import pickle
 import numpy as np
 import json
 
-IMG_WIDTH_HEIGHT = (256, 256)
+#IMG_WIDTH_HEIGHT = (256, 256)
+img_size = (256, 256)
 
 # Fonctions loss
 def dice_coeff(y_true, y_pred):
@@ -48,13 +51,26 @@ def home():
         # Retourner le fichier sélectionné
         image = request.form['image']
         image_path = str('data/test/images/' + image)
- 
+        
+        with open(image_path, "rb") as f:
+            original_img_b64 = base64.b64encode(f.read())
+            original_img_b64_str = original_img_b64.decode("utf-8")
+        
+            input_img = Image.open(
+            BytesIO(base64.b64decode(original_img_b64))
+        ).resize(img_size)
+        
+            pred_mask = np.squeeze(
+                    np.argmax(
+                        model.predict(np.expand_dims(input_img, 0)), axis=-1
+                    )
+                )
         #image=os.path.join(app.config['UPLOAD_FOLDER'],image)
-        X = img_to_array(load_img(image_path, target_size=(IMG_WIDTH_HEIGHT)))/255
-        X = np.expand_dims(X, 0)
-        prediction = model.predict(X)
-        pred_mask = np.argmax(prediction, axis=-1)
-        pred_mask = np.squeeze(pred_mask)
+        #X = img_to_array(load_img(image_path, target_size=(IMG_WIDTH_HEIGHT)))/255
+        #X = np.expand_dims(X, 0)
+        #prediction = model.predict(X)
+        #pred_mask = np.argmax(prediction, axis=-1)
+        #pred_mask = np.squeeze(pred_mask)
         print(pred_mask)
         
     return render_template('index.html',prediction=pred_mask)
